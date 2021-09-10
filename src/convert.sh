1f32063911411c3374c0ddfe75ccd080b33192cb
HeikoMatternTK <heiko.mattern@turbinekreuzberg.com>
2021-09-10 13:08:24 +0200

    Added shell scripts

---
+++ src/convert.sh
@@ -0,0 +1,59 @@
+ffmpeg -i 001_HereCome.webm -strict experimental 001_HereCome.mp3
+ffmpeg -i 001_HereCome1.webm -strict experimental 001_HereCome1.mp3
+ffmpeg -i 001_HereCome2.webm -strict experimental 001_HereCome2.mp3
+
+ffmpeg -i 002_HeGot.webm -strict experimental 002_HeGot.mp3
+ffmpeg -i 002_HeGot1.webm -strict experimental 002_HeGot1.mp3
+ffmpeg -i 002_HeGot2.webm -strict experimental 002_HeGot2.mp3
+
+ffmpeg -i 003_Hair.webm -strict experimental 003_Hair.mp3
+ffmpeg -i 003_Hair1.webm -strict experimental 003_Hair1.mp3
+ffmpeg -i 003_Hair2.webm -strict experimental 003_Hair2.mp3
+
+ffmpeg -i 004_ShoeShine.webm -strict experimental 004_ShoeShine.mp3
+ffmpeg -i 004_ShoeShine1.webm -strict experimental 004_ShoeShine1.mp3
+ffmpeg -i 004_ShoeShine2.webm -strict experimental 004_ShoeShine2.mp3
+
+ffmpeg -i 005_MonkeyFinger.webm -strict experimental 005_MonkeyFinger.mp3
+ffmpeg -i 005_MonkeyFinger1.webm -strict experimental 005_MonkeyFinger1.mp3
+ffmpeg -i 005_MonkeyFinger2.webm -strict experimental 005_MonkeyFinger2.mp3
+
+ffmpeg -i 006_IKnowYou.webm -strict experimental 006_IKnowYou.mp3
+ffmpeg -i 006_IKnowYou1.webm -strict experimental 006_IKnowYou1.mp3
+ffmpeg -i 006_IKnowYou2.webm -strict experimental 006_IKnowYou2.mp3
+
+ffmpeg -i 007_OneThing.webm -strict experimental 007_OneThing.mp3
+ffmpeg -i 007_OneThing1.webm -strict experimental 007_OneThing1.mp3
+ffmpeg -i 007_OneThing2.webm -strict experimental 007_OneThing2.mp3
+
+ffmpeg -i 008_Ref_01.webm -strict experimental 008_Ref_01.mp3
+ffmpeg -i 008_Ref_011.webm -strict experimental 008_Ref_011.mp3
+ffmpeg -i 008_Ref_012.webm -strict experimental 008_Ref_012.mp3
+
+ffmpeg -i 009_BagProduction.webm -strict experimental 009_BagProduction.mp3
+ffmpeg -i 009_BagProduction1.webm -strict experimental 009_BagProduction1.mp3
+ffmpeg -i 009_BagProduction2.webm -strict experimental 009_BagProduction2.mp3
+
+ffmpeg -i 010_OnoSidevboard.webm -strict experimental 010_OnoSidevboard.mp3
+ffmpeg -i 010_OnoSidevboard1.webm -strict experimental 010_OnoSidevboard1.mp3
+ffmpeg -i 010_OnoSidevboard2.webm -strict experimental 010_OnoSidevboard2.mp3
+
+ffmpeg -i 011_FeetDonwBelow.webm -strict experimental 011_FeetDonwBelow.mp3
+ffmpeg -i 011_FeetDonwBelow1.webm -strict experimental 011_FeetDonwBelow1.mp3
+ffmpeg -i 011_FeetDonwBelow2.webm -strict experimental 011_FeetDonwBelow2.mp3
+
+ffmpeg -i 012_RollerCoaster.webm -strict experimental 012_RollerCoaster.mp3
+ffmpeg -i 012_RollerCoaster1.webm -strict experimental 012_RollerCoaster1.mp3
+ffmpeg -i 012_RollerCoaster2.webm -strict experimental 012_RollerCoaster2.mp3
+
+ffmpeg -i 013_MuddyWater.webm -strict experimental 013_MuddyWater.mp3
+ffmpeg -i 013_MuddyWater1.webm -strict experimental 013_MuddyWater1.mp3
+ffmpeg -i 013_MuddyWater2.webm -strict experimental 013_MuddyWater2.mp3
+
+ffmpeg -i 014_OneAndOne.webm -strict experimental 014_OneAndOne.mp3
+ffmpeg -i 014_OneAndOne1.webm -strict experimental 014_OneAndOne1.mp3
+ffmpeg -i 014_OneAndOne2.webm -strict experimental 014_OneAndOne2.mp3
+
+ffmpeg -i 015_ComeTogehterYeah.webm -strict experimental 015_ComeTogehterYeah.mp
+ffmpeg -i 015_ComeTogehterYeah1.webm -strict experimental 015_ComeTogehterYeah1.mp
+ffmpeg -i 015_ComeTogehterYeah2.webm -strict experimental 015_ComeTogehterYeah2.mp
